ec2f0111bd022cb3f89969fea0fbf8036a73a3c4
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-07-03 15:59:54 -0400

    table changes - add image table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,16 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/c8d82705-baee-4d8b-ab40-f93337a73a08-9vmive.avif",
-  "https://utfs.io/f/6a0bdd85-16ec-4d73-badd-3515d08b4238-ehdy0w.jpg"
-]
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url
-})) 
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany(); 
-  
+  const images = await db.query.images.findMany({
+    orderBy: (images, {desc}) => [desc(images.id)]
+  }); 
 
   return (
     <main className="">
-      <h3 className="text-xl">posts</h3>
-      <div>{posts.map(post => (<span key={post.id}>{post.name}</span>))}</div>
-      {/* <code>{JSON.stringify(posts)}</code> */}
-      <br/>
       <div className="flex flex-wrap gap-4">
         {
-          [...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          [...images, ...images, ...images, ...images].map((image, index) => (
             <div key={`${image.id}-${index}`} className="w-48">
               <img src={image.url} alt="image" />
             </div>
